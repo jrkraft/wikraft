66112a449357de7e7406b1a91691717ee9a16823
Guy Lerat <guy.lerat.jr@gmail.com>
2015-06-17 04:45:22 +0200

    ajout de fichiers suite a l implementation dans eclipse

--- HelloJenkinsTest/src/main/java/conteneur/App.java
+++ HelloJenkinsTest/src/main/java/conteneur/App.java
@@ -1,4 +1,4 @@
-package package;
+package conteneur;
 
 /**
  * Hello world!
--- HelloJenkinsTest/src/test/java/conteneurtest/AppTest.java
+++ HelloJenkinsTest/src/test/java/conteneurtest/AppTest.java
@@ -1,4 +1,4 @@
-package package;
+package conteneurtest;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
